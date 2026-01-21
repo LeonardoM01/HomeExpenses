1dbb82e4bc2477cc00a9b6fea8756b5da571d352
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-01-21 03:28:38 -0300

    Criação dos controllers

---
+++ HomeExpenses.Server/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using HomeExpenses.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+using HomeExpenses.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeExpenses.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController: ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return category;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategory), new { id = category.Id },category);
+        }        
+    }
+}
---
+++ HomeExpenses.Server/Controllers/TransactionController.cs
@@ -0,0 +1,45 @@
+﻿using HomeExpenses.Server.Data;
+using HomeExpenses.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeExpenses.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase 
+    {
+        private readonly AppDbContext _context;
+
+        public TransactionController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaction>> GetTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            return transaction;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> CreateTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTransaction), new { id = transaction.Id }, transaction);
+        }
+    }
+}
