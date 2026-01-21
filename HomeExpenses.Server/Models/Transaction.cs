27f6e92261359d3385279d9fdd3ffdbf2d63920e
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-01-21 03:29:42 -0300

    Criação das classes (models)

---
+++ HomeExpenses.Server/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeExpenses.Server.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public enum TransactionType
+        {
+            Expense,
+            Income
+        }
+        public Category Category { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
---
+++ HomeExpenses.Server/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeExpenses.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
