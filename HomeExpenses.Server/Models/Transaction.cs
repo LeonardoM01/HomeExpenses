fa52a0cd527fe6e1ec455e9651ab985a9be14877
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-02-04 23:29:50 -0300

    Classe e Controller de Transações

--- HomeExpenses.Server/Models/Transaction.cs
+++ HomeExpenses.Server/Models/Transaction.cs
@@ -1,16 +1,54 @@
-﻿namespace HomeExpenses.Server.Models
+﻿using System.Text.Json.Serialization;
+
+namespace HomeExpenses.Server.Models
 {
     public class Transaction
     {
         public int Id { get; set; }
         public string Description { get; set; } = string.Empty;
         public decimal Amount { get; set; }
-        public enum TransactionType
-        {
-            Expense,
-            Income
-        }
+
+        public TransactionType TransactionType { get; set; }
+
+        public int CategoryId { get; set; }
         public Category Category { get; set; } = null!;
+
+        public int UserId { get; set; }
         public User User { get; set; } = null!;
+    }
+
+    public class TransactionCreateDto
+    {
+        public string Description { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+        public TransactionType TransactionType { get; set; }
+
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class UserSummary
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal TotalExpenses { get; set; }
+        public decimal TotalRevenues { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+    public class CategorySummary
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal TotalExpenses { get; set; }
+        public decimal TotalRevenues { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TransactionType
+    {
+        Revenue,
+        Expense
     }
 }
